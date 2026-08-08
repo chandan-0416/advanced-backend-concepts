b4c383cea5f880967799d011ad89fa9eaa055bfd
Chandan Kushwaha <chandankushwaha0411@gmail.com>
2026-08-09 00:30:54 +0530

    AI Concepts: LLM, Lang chain, Lang graph

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
-# Advanced Backend Concepts with AI
+### Advanced Backend Concepts with AI
+🚀 Production-ready MERN + AI-powered application featuring Microservices Architecture, Multi-Agent AI System, LangGraph, LangChain, RAG, Qdrant Vector DB, Redis caching, JWT authentication, Docker, and AWS deployment.
 1. Level-1 : Docker
 2. Level-2 : Redis
 3. Level-3 : System Design (Load balancer - nginx, API Gateway)
--- level-4/index.js
+++ level-4/index.js
@@ -1,32 +1,66 @@
 import express from "express";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
+import { ChatGroq } from "@langchain/groq"
 
 dotenv.config();
 const app = express();
 const port = 5000;
-
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
-
 app.use(express.json());
 
-//How to use in api to generate the response
+
+//without LangChain
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+// });
+
+// //How to use in api to generate the response
+// app.post("/ai", async (req, res) => {
+//   const { input } = req.body;
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3.6-flash",
+//      config: {
+//     systemInstruction: `
+//       You are a helpful and accurate AI assistant.
+//       If you do not know the answer, clearly say that you don't know.
+//       Do not make up or hallucinate information.
+//       If the question requires information you cannot verify, say so.
+//     `,
+//   },
+//     contents: [  
+//           {
+//             role: "user",
+//             parts: [{ text: input }],
+//           },
+//     ],
+//   });
+//   return res.status(200).json({ "ai:": response.text });
+// });
+
+//with LangChain
+const llm= new ChatGroq({
+  model:"openai/gpt-oss-120b",
+  temperature:2,
+  maxTokens:150,
+  maxRetries:2,
+})
+
 app.post("/ai", async (req, res) => {
-  const { input } = req.body;
-  const response = await ai.models.generateContent({
-    model: "gemini-3.6-flash",
+  const { input } = req.body
+   
+  const response = await llm.invoke([
+    {
+      role:"system",
+      content:"you are AI"
+    },
+    {
+      role:"human",
+      content:input
+    }
+  ])
 
-
-    contents: [      
-          {
-            role: "user",
-            parts: [{ text: input }],
-          },
-    ],
-  });
-  return res.status(200).json({ "ai:": response.text });
+  return res.status(200).json({ "ai:": response.content });
 });
 
 app.get("/", (req, res) => {
